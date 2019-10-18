87dceaf0d63bf6bdb070e94d632183b62aa7850b
Lucas Teixeira <lucasxteixeira@gmail.com>
2019-10-18 07:46:46 -0300

    Update LSTM to use TimeseriesGenerator and fix KFold with TimeSeriesSplit

--- lstm.py
+++ lstm.py
@@ -4,6 +4,7 @@ import warnings
 import pandas as pd
 import numpy as np
 import tensorflow as tf
+import math
 
 from sklearn.metrics import (
     accuracy_score,
@@ -12,12 +13,12 @@ from sklearn.metrics import (
     recall_score,
     confusion_matrix,
 )
-from sklearn.model_selection import train_test_split, KFold
+from sklearn.model_selection import TimeSeriesSplit
 from keras.models import Sequential
-from keras.layers import Dense, Embedding, LSTM
+from keras.layers import Dense, Embedding, LSTM, Dropout
 from keras.optimizers import RMSprop
+from keras.preprocessing.sequence import TimeseriesGenerator
 from keras import backend as K
-
 
 
 def f1(y_true, y_pred):
@@ -52,13 +53,13 @@ def f1(y_true, y_pred):
     return 2 * ((precision * recall) / (precision + recall + K.epsilon()))
 
 
+history_length = 50
 batch_size = 128
 epochs = 10
-embed_dim = 127
+n_features = 127
 
 # read in data using pandas
 df = pd.read_csv("dataset/final_dataset.csv")
-
 
 # create a dataframe with all training data except the target column
 X = df.drop(columns=["PKT_CLASS"])
@@ -83,35 +84,51 @@ Y = pd.concat([dummy], axis=1)
 seed = 4
 np.random.seed(seed)
 
-kf = KFold(n_splits=5, shuffle=True, random_state=seed)
+# Use special splitter for time series
+# Explanation: https://i.stack.imgur.com/fXZ6k.png
+kf = TimeSeriesSplit(n_splits=5)
 kf.get_n_splits(X)
 
 count_kfold = 1
 cvscores = []
 
 for train_index, test_index in kf.split(X, Y):
+
     X_train, X_test = X.iloc[train_index], X.iloc[test_index]
     Y_train, Y_test = Y.iloc[train_index], Y.iloc[test_index]
-    # create model
 
+    # Get the numpy array
+    X_train = X_train.values
+    X_test = X_test.values
+    Y_train = Y_train.values
+    Y_test = Y_test.values
+
+    # X_train and Y_train out of sync!
+    Y_train = np.roll(Y_train, 1, axis = 0)
+
+    # Convert the data to a 3d array
+    generator = TimeseriesGenerator(X_train, Y_train, length = history_length, batch_size = batch_size)
+
+    # create model
     print("kfold: ", count_kfold)
     model = Sequential()
 
     # add model layers
-    model.add(Embedding(2500, embed_dim, input_length = X.shape[1]))
-    model.add(LSTM(200, dropout = 0.2, recurrent_dropout = 0.2))
+    model.add(LSTM(units=200, dropout=0.2, recurrent_dropout=0.2, input_shape=(history_length, n_features)))
     model.add(Dense(5,activation='softmax'))
-    print(model.summary())
 
     # compile model using mse as a measure of model performance
     model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy", f1])
 
+    # fit_generator works differently from fit: https://stackoverflow.com/questions/43457862/whats-the-difference-between-samples-per-epoch-and-steps-per-epoch-in-fit-g
+    # So in order to use the whole dataset we need to calculate the exact number of steps per epoch in order to fully pass the entire dataset each epoch
+    steps_epoch = math.floor((len(X_train) - history_length)/batch_size)
+
     # train model
-    model.fit(
-        X_train,
-        Y_train,
-        batch_size=batch_size,
+    model.fit_generator(
+        generator,
         verbose=1,
+        steps_per_epoch=steps_epoch,
         epochs=epochs,
     )
 
