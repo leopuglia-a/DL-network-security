5496c9565b7647b01cd4170bfe7c28f0a1b54aad
Leonardo Puglia <leopuglia.a@gmail.com>
2019-11-18 09:24:13 -0300

    update git

--- gru.py
+++ gru.py
@@ -4,7 +4,8 @@ import warnings
 import pandas as pd
 import numpy as np
 import tensorflow as tf
-
+import math
+import matplotlib.pyplot as plt
 from sklearn.metrics import (
     accuracy_score,
     f1_score,
@@ -12,13 +13,14 @@ from sklearn.metrics import (
     recall_score,
     confusion_matrix,
 )
-from sklearn.model_selection import train_test_split, KFold
+from sklearn.model_selection import TimeSeriesSplit
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Embedding, GRU, Dropout
 from keras.optimizers import RMSprop
+from keras.preprocessing.sequence import TimeseriesGenerator
 from keras import backend as K
-
-
+from keras.utils import plot_model
+from keras.callbacks import CSVLogger
 
 def f1(y_true, y_pred):
     def recall(y_true, y_pred):
@@ -52,12 +54,13 @@ def f1(y_true, y_pred):
     return 2 * ((precision * recall) / (precision + recall + K.epsilon()))
 
 
-batch_size = 128
-epochs = 30
+history_length = 50
+batch_size = 256
+epochs = 10
+n_features = 127
 
 # read in data using pandas
 df = pd.read_csv("dataset/final_dataset.csv")
-
 
 # create a dataframe with all training data except the target column
 X = df.drop(columns=["PKT_CLASS"])
@@ -77,53 +80,86 @@ dummy = pd.get_dummies(Y["PKT_CLASS"])
 Y = Y.drop(columns=["PKT_CLASS"])
 Y = pd.concat([dummy], axis=1)
 
-
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-
 # KFOLD
 seed = 7
 np.random.seed(seed)
 
-kf = KFold(n_splits=5, shuffle=True, random_state=seed)
+# Use special splitter for time series
+# Explanation: https://i.stack.imgur.com/fXZ6k.png
+kf = TimeSeriesSplit(n_splits=5)
 kf.get_n_splits(X)
 
 count_kfold = 1
 cvscores = []
+csv_logger = CSVLogger('lstm.csv', append=True, separator=';')
 
 for train_index, test_index in kf.split(X, Y):
+
     X_train, X_test = X.iloc[train_index], X.iloc[test_index]
     Y_train, Y_test = Y.iloc[train_index], Y.iloc[test_index]
-    # create model
 
+    # Get the numpy array
+    X_train = X_train.values
+    X_test = X_test.values
+    Y_train = Y_train.values
+    Y_test = Y_test.values
+
+    # X_train and Y_train out of sync!
+    Y_train = np.roll(Y_train, 1, axis = 0)
+    Y_test = np.roll(Y_test, 1, axis = 0)
+    
+    # Convert the data to a 3d array
+    generator = TimeseriesGenerator(X_train, Y_train, length = history_length, batch_size = batch_size)
+
+    # create model
     print("kfold: ", count_kfold)
     model = Sequential()
 
     # add model layers
-    model.add(Dense(2048, activation="relu", input_dim=(87)))
-    model.add(Dense(1024, activation="relu"))
-    model.add(Dense(512, activation="relu"))
-    model.add(Dense(256, activation="relu"))
-    model.add(Dense(128, activation="relu"))
-    model.add(Dense(64, activation="relu"))
-    model.add(Dense(1, activation="sigmoid"))
+    model.add(GRU(units=200, dropout=0.2, recurrent_dropout=0.2, input_shape=(history_length, n_features)))
+    model.add(Dense(5,activation='softmax'))
     model.summary()
 
     # compile model using mse as a measure of model performance
-    model.compile(
-        loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy", f1]
-    )
+    model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy", f1])
+
+    # fit_generator works differently from fit: https://stackoverflow.com/questions/43457862/whats-the-difference-between-samples-per-epoch-and-steps-per-epoch-in-fit-g
+    # So in order to use the whole dataset we need to calculate the exact number of steps per epoch in order to fully pass the entire dataset each epoch
+    steps_epoch = math.floor((len(X_train) - history_length)/batch_size)
+
+    # csv_logger = CSVLogger('lstm.csv', append=True, separator=';')
 
     # train model
-    model.fit(
-        X_train,
-        Y_train,
-        batch_size=batch_size,
-        verbose=0,
+    hist = model.fit_generator(
+        generator,
+        verbose=1,
+        steps_per_epoch=steps_epoch,
         epochs=epochs,
+        callbacks=[csv_logger]
     )
 
-    scores = model.evaluate(X_test, Y_test, verbose=0)
+    generator = TimeseriesGenerator(X_test, Y_test, length = history_length, batch_size = batch_size)
+    scores = model.evaluate_generator(generator, verbose=0)
     print(model.metrics_names)
     print("%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
     print("%s: %.2f%%" % (model.metrics_names[2], scores[2] * 100))
     count_kfold += 1
+
+
+plot_model(model, to_file='model.png')
+
+plt.plot(hist.history['accuracy'])
+plt.title('Model accuracy')
+plt.ylabel('Accuruacy')
+plt.xlabel('Epoch')
+plt.legend(['acc'], loc='upper right')
+plt.savefig('acc.png')
+# plt.show()
+
+plt.plot(hist.history['f1'])
+plt.title('Model f1')
+plt.ylabel('f1')
+plt.xlabel('Epoch')
+plt.legend(['f1'], loc='upper right')
+plt.savefig('f1.png')
+# plt.show()
