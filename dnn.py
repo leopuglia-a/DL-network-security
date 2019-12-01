3854936138648d3bd279b1051deb84ccbc9e4610
Leonardo Puglia <leopuglia.a@gmail.com>
2019-12-01 15:13:31 -0300

    updating Y dataframe

--- dnn.py
+++ dnn.py
@@ -81,11 +81,10 @@ training_files = [
 
 
 testing_files = [
+'./dataset/testing/LDAP.csv',
 './dataset/testing/MSSQL.csv',
 './dataset/testing/NetBIOS.csv',
 './dataset/testing/Syn.csv',
-'./dataset/testing/UDP.csv',
-'./dataset/testing/UDPLag.csv',
 './dataset/testing/Portmap.csv'
 ]
 
@@ -94,12 +93,11 @@ def concat(file, list):
     for index, f in enumerate(list):
         print("reading file ", f)
         if index == 0:
-            print("first")
-            df = pd.read_csv(f, skipinitialspace=True, low_memory=False)
+            df = pd.read_csv(f, skipinitialspace=True, low_memory=False, nrows=100000)
             df.to_csv(file, index=False)
-            continue
-        df = pd.read_csv(f, skiprows=1, skipinitialspace=True, index_col=False, low_memory=False)
-        df.to_csv(file, mode='a', index=False, header=False)
+        else:
+            df = pd.read_csv(f, skiprows=1, skipinitialspace=True, low_memory=False, nrows=100000)
+            df.to_csv(file, mode='a', index=False, header=False)
 
 
 concat('./dataset/training-ds.csv', training_files)
@@ -125,8 +123,6 @@ X_train = df.drop(['Label'], axis=1)
 indices_to_keep = ~X_train.isin([np.nan, np.inf, -np.inf]).any(1)
 X_train = X_train[indices_to_keep].astype(np.float32)
 
-print(X_train)
-
 Y_train = df[["Label"]]
 Y_train = Y_train[indices_to_keep]
 dummy = pd.get_dummies(Y_train["Label"])
@@ -134,13 +130,46 @@ Y_train = Y_train.drop(columns=["Label"])
 Y_train = pd.concat([dummy], axis=1)
 Y_train["Portmap"] = 0
 
+concat('./dataset/testing-ds.csv', testing_files)
+
+df = pd.read_csv('./dataset/testing-ds.csv', low_memory=False)
+
+df.columns = (df.columns.str.replace("^ ", "")).str.replace(" $", "")
+df['Timestamp'] = df['Timestamp'].apply(lambda x: utils.date_str_to_ms(x))
+
+# create a dataframe with all training data except the target column
+df = df.drop(['Flow ID', 'Source IP', 'Destination IP', 'SimillarHTTP'], axis=1)
+df['Flow Bytes/s'] = df['Flow Bytes/s'].astype(np.float32)
+df['Flow Packets/s'] = df['Flow Packets/s'].astype(np.float32)
+
+# Drop rows that have NA, NaN or Inf
+df.dropna(inplace=True)
+
+# Remove the Label output
+X_test = df.drop(['Label'], axis=1)
+X_test.dropna(inplace=True)
+indices_to_keep = ~df.isin([np.nan, np.inf, -np.inf]).any(1)
+X_test = X_test[indices_to_keep].astype(np.float32)
 
 # create a dataframe with only the target column
-print("class dataframe")
-print(Y_train)
+Y_test = df[["Label"]]
+Y_test = Y_test[indices_to_keep]
+dummy = pd.get_dummies(Y_test["Label"])
+Y_test = Y_test.drop(columns=["Label"])
+Y_test = pd.concat([dummy], axis=1)
+Y_test.columns = ['BENIGN', 'DrDoS_LDAP', 'DrDoS_MSSQL', 'DrDoS_NetBIOS', 'Portmap', 'Syn']
+Y_test = Y_test[['BENIGN', 'DrDoS_LDAP', 'DrDoS_MSSQL', 'DrDoS_NetBIOS', 'Syn', 'Portmap']]
+Y_test.insert(1, 'DrDoS_DNS', 0)
+Y_test.insert(4, 'DrDoS_NTP', 0)
+Y_test.insert(6, 'DrDoS_SNMP', 0)
+Y_test.insert(7, 'DrDoS_SSDP', 0)
+Y_test.insert(8, 'DrDoS_UDP', 0)
+Y_test.insert(10, 'TFTP', 0)
+Y_test.insert(11, 'UDP-lag', 0)
+Y_test.insert(12, 'WebDDoS', 0)
 
 # loggers
-csv_logger = CSVLogger('lstm.csv', append=True, separator=';')
+csv_logger = CSVLogger('dnn.csv', append=True, separator=';')
 
 # scaler = preprocessing.StandardScaler()
 
@@ -201,34 +230,6 @@ model.fit(
     epochs=utils.epochs,
 )
 
-
-concat('./dataset/testing/LDAP.csv', testing_files)
-
-df = pd.read_csv('./dataset/testing-ds.csv', low_memory=False)
-
-df.columns = (df.columns.str.replace("^ ", "")).str.replace(" $", "")
-df['Timestamp'] = df['Timestamp'].apply(lambda x: utils.date_str_to_ms(x))
-
-# create a dataframe with all training data except the target column
-df = df.drop(['Flow ID', 'Source IP', 'Destination IP', 'SimillarHTTP'], axis=1)
-df['Flow Bytes/s'] = df['Flow Bytes/s'].astype(np.float32)
-df['Flow Packets/s'] = df['Flow Packets/s'].astype(np.float32)
-
-# Drop rows that have NA, NaN or Inf
-df.dropna(inplace=True)
-
-# Remove the Label output
-X_test = df.drop(['Label'], axis=1)
-X_test.dropna(inplace=True)
-indices_to_keep = ~df.isin([np.nan, np.inf, -np.inf]).any(1)
-X_test = X_test[indices_to_keep].astype(np.float32)
-
-# create a dataframe with only the target column
-
-Y_test = df[["Label"]]
-dummy = pd.get_dummies(Y_test["Label"])
-Y_test = Y_test.drop(columns=["Label"])
-Y_test = pd.concat([dummy], axis=1)
 
 scores = model.evaluate(X_test, Y_test, verbose=1)
 print("\n============= FINAL SCORE ============\n")
