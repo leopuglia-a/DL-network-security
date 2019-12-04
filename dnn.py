3e8039971ca3341946d0d3032bf5b1b006f3bdb2
Lucas Teixeira <lucasxteixeira@gmail.com>
2019-12-04 10:36:12 -0300

    Fix code for testing

--- dnn.py
+++ dnn.py
@@ -28,9 +28,7 @@ csv_logger = CSVLogger('dnn2.csv', append=True, separator=';')
 #### F1 Function ####
 def recall(y_true, y_pred):
     """Recall metric.
-
     Only computes a batch-wise average of recall.
-
     Computes the recall, a metric for multi-label classification of
     how many relevant items are selected.
     """
@@ -41,9 +39,7 @@ def recall(y_true, y_pred):
 
 def precision(y_true, y_pred):
     """Precision metric.
-
     Only computes a batch-wise average of precision.
-
     Computes the precision, a metric for multi-label classification of
     how many selected items are relevant.
     """
@@ -63,10 +59,10 @@ def concat(file, list):
     for index, f in enumerate(list):
         print("reading file ", f)
         if index == 0:
-            df = pd.read_csv(f, skipinitialspace=True, low_memory=False, nrows=500000)
+            df = pd.read_csv(f, skipinitialspace=True, low_memory=False, nrows=100000)
             df.to_csv(file, index=False)
         else:
-            df = pd.read_csv(f, skiprows=1, skipinitialspace=True, low_memory=False, nrows=500000)
+            df = pd.read_csv(f, skiprows=1, skipinitialspace=True, low_memory=False, nrows=100000)
             df.to_csv(file, mode='a', index=False, header=False)
 
 
@@ -94,6 +90,7 @@ testing_files = [
 csv_logger = CSVLogger('dnn.csv', append=True, separator=';')
 
 # concat('./dataset/training-ds.csv', training_files)
+# concat('./dataset/testing-ds.csv', testing_files)
 
 
 #### Read data training ####
@@ -105,7 +102,7 @@ df.columns = (df.columns.str.replace("^ ", "")).str.replace(" $", "")
 # Remove columns that don't add useful information
 df = df.drop(['Unnamed: 0', 'Flow ID', 'Source IP', 'Destination IP', 'SimillarHTTP', 'Timestamp'], axis=1)
 
-# # Convert Timestamp column to usable values 
+# # Convert Timestamp column to usable values
 # df['Timestamp'] = df['Timestamp'].apply(lambda x: utils.date_str_to_ms(x))
 
 # Cast the variables to correct types
@@ -128,49 +125,12 @@ Y_train = df[["Label"]]
 dummy = pd.get_dummies(Y_train["Label"])
 Y_train = Y_train.drop(columns=["Label"])
 Y_train = pd.concat([dummy], axis=1)
-print(Y_train)
 Y_train = Y_train.values
 
 # Standardize the input variables
 scaler = preprocessing.StandardScaler()
 X_train = scaler.fit_transform(X_train)
 
-
-# concat('./dataset/testing-ds.csv', testing_files)
-df = pd.read_csv('./dataset/testing-ds.csv',low_memory=False)
-
-df.columns = (df.columns.str.replace("^ ", "")).str.replace(" $", "")
-
-df = df.drop(['Unnamed: 0', 'Flow ID', 'Source IP', 'Destination IP', 'SimillarHTTP', 'Timestamp'], axis=1)
-
-# Convert Timestamp column to usable values 
-# df['Timestamp'] = df['Timestamp'].apply(lambda x: utils.date_str_to_ms(x))
-
-df['Flow Bytes/s'] = df['Flow Bytes/s'].astype(np.float32)
-df['Flow Packets/s'] = df['Flow Packets/s'].astype(np.float32)
-
-df.dropna(inplace=True)
-
-df = df.sample(frac=1).reset_index(drop=True)
-Y_test = df[["Label"]]
-X_test = df.drop(['Label'], axis=1).values
-
-dummy = pd.get_dummies(Y_test["Label"])
-Y_test = Y_test.drop(columns=["Label"])
-Y_test = pd.concat([dummy], axis=1)
-Y_test.columns = ['BENIGN', 'DrDoS_LDAP', 'DrDoS_MSSQL', 'DrDoS_NetBIOS', 'Syn']
-Y_test = Y_test[['BENIGN', 'DrDoS_LDAP', 'DrDoS_MSSQL', 'DrDoS_NetBIOS', 'Syn']]
-Y_test.insert(1, 'DrDoS_DNS', 0)
-Y_test.insert(4, 'DrDoS_NTP', 0)
-Y_test.insert(6, 'DrDoS_SNMP', 0)
-Y_test.insert(7, 'DrDoS_SSDP', 0)
-Y_test.insert(8, 'DrDoS_UDP', 0)
-Y_test.insert(10, 'TFTP', 0)
-Y_test.insert(11, 'UDP-lag', 0)
-Y_test.insert(12, 'WebDDoS', 0)
-
-print(Y_test)
-Y_test = Y_test.values
 
 # Create the sequential model
 print("\n\n")
@@ -198,7 +158,6 @@ model.add(BatchNormalization())
 model.add(Activation('relu'))
 model.add(Dropout(0.01))
 
-# kernel_regularizer=l2(0.001), activity_regularizer=l2(0.001)))
 model.add(Dense(13, activation="softmax"))
 model.summary()
 
@@ -207,6 +166,7 @@ opt = optimizers.Adam(learning_rate=0.00001)
 model.compile(
     loss="categorical_crossentropy", optimizer=opt, metrics=['accuracy', precision, recall, f1]
 )
+
 history = model.fit(
     X_train,
     Y_train,
@@ -216,30 +176,6 @@ history = model.fit(
     callbacks=[csv_logger]
 )
 
-scores = model.evaluate(X_test, Y_test, verbose=1)
-
-
-plt.plot(history.history['accuracy'])
-plt.plot(history.history['precision'])
-plt.plot(history.history['recall'])
-plt.plot(history.history['f1'])
-plt.title('Model f1')
-plt.xlabel('Epoch')
-plt.legend(['accuracy', 'precision', 'recall', 'f1'], loc='lower right')
-plt.savefig('dnn3.png')
-
-
-
-
-
-
-# print("\n============= FINAL SCORE ============\n")
-# print(model.metrics_names)
-# print("%s: %.4f" % (model.metrics_names[0], scores[0]))
-# print("%s: %.4f" % (model.metrics_names[1], scores[1]))
-# print("%s: %.4f" % (model.metrics_names[2], scores[2]))
-# print("%s: %.4f" % (model.metrics_names[3], scores[3]))
-
 # plt.plot(history.history['accuracy'])
 # plt.plot(history.history['precision'])
 # plt.plot(history.history['recall'])
@@ -247,4 +183,57 @@ plt.savefig('dnn3.png')
 # plt.title('Model f1')
 # plt.xlabel('Epoch')
 # plt.legend(['accuracy', 'precision', 'recall', 'f1'], loc='lower right')
-# plt.savefig('dnn-test.png')
+# plt.savefig('dnn3.png')
+
+
+
+
+
+#### Testing ####
+df = pd.read_csv('./dataset/testing-ds.csv',low_memory=False)
+
+# Sanity check empty entries
+df.columns = (df.columns.str.replace("^ ", "")).str.replace(" $", "")
+
+# Remove columns that don't add useful information
+df = df.drop(['Unnamed: 0', 'Flow ID', 'Source IP', 'Destination IP', 'SimillarHTTP', 'Timestamp'], axis=1)
+
+# # Convert Timestamp column to usable values
+# df['Timestamp'] = df['Timestamp'].apply(lambda x: utils.date_str_to_ms(x))
+
+# Cast the variables to correct types
+df['Flow Bytes/s'] = df['Flow Bytes/s'].astype(np.float32)
+df['Flow Packets/s'] = df['Flow Packets/s'].astype(np.float32)
+
+# Drop rows that have NA, NaN or Inf
+df.dropna(inplace=True)
+indices_to_keep = ~df.isin([np.nan, np.inf, -np.inf]).any(1)
+df = df[indices_to_keep]
+
+# Shuffle the dataset
+df = df.sample(frac=1).reset_index(drop=True)
+
+# Extract the test columns and convert it to a plain numpy array
+X_test = df.drop(['Label'], axis=1).values
+
+# Extract the test column, dummy it and convert it to a plain numpy array
+Y_test = df[["Label"]]
+dummy = pd.get_dummies(Y_test["Label"])
+Y_test = Y_test.drop(columns=["Label"])
+Y_test = pd.concat([dummy], axis=1)
+Y_test.columns = ['BENIGN', 'DrDoS_LDAP', 'DrDoS_MSSQL', 'DrDoS_NetBIOS', 'Syn']
+Y_test.insert(1, 'DrDoS_DNS', 0)
+Y_test.insert(4, 'DrDoS_NTP', 0)
+Y_test.insert(6, 'DrDoS_SNMP', 0)
+Y_test.insert(7, 'DrDoS_SSDP', 0)
+Y_test.insert(8, 'DrDoS_UDP', 0)
+Y_test.insert(10, 'TFTP', 0)
+Y_test.insert(11, 'UDP-lag', 0)
+Y_test.insert(12, 'WebDDoS', 0)
+Y_test = Y_test.values
+
+# Standardize the input variables
+scaler = preprocessing.StandardScaler()
+X_test = scaler.fit_transform(X_test)
+
+scores = model.evaluate(X_test, Y_test, verbose=0)
